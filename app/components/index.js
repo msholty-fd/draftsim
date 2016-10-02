e6d58da64df0bdc31b04a128da57bb6b62b41f07
Michael Sholty <michael.sholty@fanduel.com>
2016-10-02 12:58:27 -0400

    refactor: fix module name

--- app/components/index.js
+++ app/components/index.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import globalHeader from './global-header';
 import globalFooter from './global-footer';
 
-export default angular.module('roulette.components', [
+export default angular.module('ds.components', [
     globalHeader.name,
     globalFooter.name
 ]);
