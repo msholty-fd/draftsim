5b141eb657595c6510de1112bfc65eb0689aa2d1
Michael Sholty <michael.sholty@fanduel.com>
2016-10-09 16:23:58 -0400

    refactor: remove snackbar controller

--- app/components/snackbar/index.js
+++ app/components/snackbar/index.js
@@ -2,15 +2,11 @@ import angular from 'angular';
 
 import SnackbarService from './snackbar-service';
 
-import SnackbarController from './snackbar-controller';
-
 import template from './snackbar.html';
 
 export default angular.module('ds.components.snackbar', [])
 
 .component('snackbar', {
-    controller: SnackbarController,
-    controllerAs: 'snackbar',
     template
 })
 
