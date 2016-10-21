bf0e45a3cf6e06b61f12272a4ff4f8538213ff97
Michael Sholty <mike.sholty@me.com>
2016-10-21 08:30:26 -0400

    refactor(ai): change off color penalty to be negative number

--- app/components/players/ai/ai-constants.js
+++ app/components/players/ai/ai-constants.js
@@ -7,7 +7,7 @@ export default {
     }, //Always commit to colors by pack 2 pick 4
     MAX_BONUS_SPEC: .9, //The maximum bonus during the speculation phase at the start of a draft
     ON_COLOR_BONUS: 2.0, //Bonus cards receive after player locks into 2 colors
-    OFF_COLOR_PENALTY: 1.0, //Penalty for off color cards after the player locks into 2 colors
+    OFF_COLOR_PENALTY: -1.0, //Penalty for off color cards after the player locks into 2 colors
     SING_COLOR_BIAS_FACTOR: 2.0, //If the player only has cards of 1 color, reduce the bonus by this fraction
     SECOND_COLOR_FRAC: 0.8, //When committed to one color, the second color bonus is this fraction of the on color bonus
     MULTICOLOR_PENALTY: 0.6, //P1P1 penalty for multicolored cards
