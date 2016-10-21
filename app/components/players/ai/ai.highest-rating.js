c6314b2bb4138f941a2c4c3533d8e9801ed880a7
Michael Sholty <mike.sholty@me.com>
2016-10-21 08:31:03 -0400

    feat: add myRating definition when updating card ratings

--- app/components/players/ai/ai.highest-rating.js
+++ app/components/players/ai/ai.highest-rating.js
@@ -1,3 +1,12 @@
 import AI from './ai';
+import _ from 'lodash';
 
-export default class HighestRatingAI extends AI {}
+export default class HighestRatingAI extends AI {
+	updateCardRatings() {
+		const unpickedCards = this.player.currentPack.unpickedCards();
+
+		_.each(unpickedCards, (card) => {
+			card.myRating = card.rating;
+		});
+	}
+}
