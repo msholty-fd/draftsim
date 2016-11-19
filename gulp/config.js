65c95d36dedb0cf9b0d2513d9854ff838f38e0ec
Michael Sholty <michael.sholty@sharpspring.com>
2016-11-19 15:30:51 -0500

    refactor(config): change aws config

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,7 @@
 const config = {};
 
 config.UI_S3_URL = '//s3.amazonaws.com/';
-config.UI_CLOUDFRONT_URL = '//d9fsetxygehvw.cloudfront.net/';
+config.UI_CLOUDFRONT_URL = '//d2jytc150hf7hn.cloudfront.net';
 config.WAM_LOCAL_PORT = 3000;
 
 config.LIVERELOAD_PORT = 35727;
@@ -11,19 +11,20 @@ config.app = './app';
 config.build = './build';
 
 config.src = {
-	html: [
-		'./app/**/*.html'
-	],
-	js: [
-		'./app/**/*.js'
-	],
-	sass: [
-		'./app/**/*.scss'
-	],
-	assets: [
-		'./app/index.html',
-		'./app/assets/**/*'
-	]
+    html: [
+        './app/**/*.html'
+    ],
+    js: [
+        './app/**/*.js'
+    ],
+    sass: [
+        './app/**/*.scss'
+    ],
+    assets: [
+        './app/index.html',
+        './scout.js',
+        './app/assets/**/*'
+    ]
 };
 
 export default config;
